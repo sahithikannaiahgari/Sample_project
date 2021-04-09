d19af27511335f0a6682b620d2d83e648ff53d6e
sahithikannaiahgari <59208403+sahithikannaiahgari@users.noreply.github.com>
2021-04-09 11:43:08 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
 import os
+def sumarray(List1):
+  sum=0
+  for i in arr:
+    sum=sum+i
+  return(sum)
 List1 = [1,2,3,4,5]
-List1.reverse()
-print("The reverselist is",List1)
+n=len(List1)
+ans=sumarray(List1)
+print("The array elements are",List1)
+print("The Sum of array elements is",ans)
